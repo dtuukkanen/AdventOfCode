7a4d84f96b2be39018ad7d52a21327c6f83221c4
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2024-11-13 15:59:23 +0200

    Add solutions for Day 13, implementing seating arrangement happiness calculation in Python

---
+++ 2015/day13/day13-1.py
@@ -0,0 +1,31 @@
+import networkx as nx
+import itertools
+
+with open('input.txt') as f:
+    # Graph for determining the best seating arrangement
+    G = nx.Graph()
+
+    data = f.read().splitlines()
+    for row in data:
+        parts = row.split(' ')
+        who = parts[0]
+        action = parts[2]
+        amount = int(parts[3])
+        if action == 'lose':
+            amount = -amount
+        who_with = parts[10][:-1]
+        if G.has_edge(who, who_with):
+            G[who][who_with]['happiness'] += amount
+        else:
+            G.add_edge(who, who_with, happiness=amount)
+
+    # Find the best seating arrangement
+    max_happiness = 0
+    for perm in itertools.permutations(G.nodes):
+        happiness = 0
+        for i in range(len(perm)):
+            happiness += G[perm[i]][perm[(i + 1) % len(perm)]]['happiness']
+        max_happiness = max(max_happiness, happiness)
+
+    print(max_happiness)
+    
