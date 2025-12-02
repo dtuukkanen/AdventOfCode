02e729f290f647ca0203c71703917438f7dce0bc
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-12-02 11:54:28 +0200

    Day 2, Puzzle 2 Completed

--- 2025/day02/puzzle.py
+++ 2025/day02/puzzle.py
@@ -1,4 +1,10 @@
-def check_range(start, end, number):
+def read_input():
+    with open('input.txt') as f:
+        line = f.readline()
+    ids = line.split(',')
+    return ids
+
+def check_range1(start, end, number):
     id_sum = 0
 
     for i in range(start, end + 1):
@@ -13,19 +19,46 @@ def check_range(start, end, number):
 
     return id_sum
 
+def check_range2(start, end, number):
+    id_sum = 0
+
+    for i in range(start, end + 1):
+        # Convert to string to split
+        str_i = str(i)
+
+        for j in range(1, len(str_i)):
+            pattern = str_i[:j]
+            repeated = pattern * (len(str_i) // len(pattern))
+            if repeated == str_i:
+                id_sum += i
+                break
+
+    return id_sum
+
 def puzzle1():
     # Read input file
     id_sum = 0
-    with open('input.txt') as f:
-        line = f.readline()
-    ids = line.split(',')
+    ids = read_input()
 
     # Go through each id range and sum valid ids
     for id in ids:
         start, end = map(int, id.split('-'))
-        id_sum += check_range(start, end, id)
+        id_sum += check_range1(start, end, id)
 
     print("ID Sum for Puzzle 1:", id_sum)
 
+def puzzle2():
+    # Read input file
+    id_sum = 0
+    ids = read_input()
+
+    # Go through each id range and sum valid ids
+    for id in ids:
+        start, end = map(int, id.split('-'))
+        id_sum += check_range2(start, end, id)
+
+    print("ID Sum for Puzzle 2:", id_sum)
+
 if __name__ == '__main__':
     puzzle1()
+    puzzle2()
