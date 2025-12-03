720f7c59b082b8e3ba9c7cfc449e962b58b0fa76
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-12-04 01:52:12 +0200

    Day 3, Puzzle 2 Completed w/ Claude

--- 2025/day03/puzzle.py
+++ 2025/day03/puzzle.py
@@ -30,5 +30,44 @@ def puzzle1():
 
     print("Puzzle 1 battery sum:", result)
 
+def puzzle2():
+    result = 0
+
+    # Read input file
+    lines = read_file()
+
+    # Process each line
+    for line in lines:
+        n = len(line)
+        k = 12  # number of batteries to keep
+
+        # Use a greedy algorithm to find the lexicographically largest subsequence
+        result_indices = []
+        start = 0
+
+        for i in range(k):
+            # We need to pick k-i more digits from position start onwards
+            # We must leave at least k-i-1 digits after our choice
+            max_digit = '0'
+            max_pos = start
+
+            # Find the largest digit in the valid range
+            for j in range(start, n - (k - i - 1)):
+                if line[j] > max_digit:
+                    max_digit = line[j]
+                    max_pos = j
+
+            result_indices.append(max_pos)
+            start = max_pos + 1
+
+        # Build the result string
+        joltage_str = ''.join([line[i] for i in result_indices])
+        joltage = int(joltage_str)
+
+        result += joltage
+
+    print("Puzzle 2 battery sum:", result)
+
 if __name__ == '__main__':
     puzzle1()
+    puzzle2()
